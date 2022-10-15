7c98d3d6f8a12d262333c0524492872497030cc2
Hju Kneyck Flores <hjucode@gmail.com>
2022-10-15 16:52:51 +0800

    fix: allow str for `created_at` & limit fields

--- app/models/log_entries.py
+++ app/models/log_entries.py
@@ -8,13 +8,11 @@ from sqlalchemy import orm
 
 class LogEntries(sqlmodel.SQLModel, table=True):
     id: Optional[pydantic.UUID4] = sqlmodel.Field(default=None, primary_key=True)  # type: ignore
-    created_at: datetime = sqlmodel.Field(  # type: ignore
-        default_factory=datetime.utcnow
-    )
-    message: str
-    level: str
-    source: str
-    raw: str
+    created_at: str = sqlmodel.Field(default_factory=datetime.utcnow, max_length=255)  # type: ignore
+    message: str = sqlmodel.Field(max_length=255)  # type: ignore
+    level: str = sqlmodel.Field(max_length=10)  # type: ignore
+    source: str = sqlmodel.Field(max_length=2048)  # type: ignore
+    raw: str = sqlmodel.Field(max_length=255)  # type: ignore
 
     @classmethod
     @orm.declared_attr

00d49b5679972d54a5c9ce982195598629d63c11
Hju Kneyck Flores <hjucode@gmail.com>
2022-11-25 23:00:40 +0800

    feat: use uuid v1 for log entry id

--- app/models/log_entries.py
+++ app/models/log_entries.py
@@ -7,7 +7,7 @@ from sqlalchemy import orm
 
 
 class LogEntries(sqlmodel.SQLModel, table=True):
-    id: Optional[pydantic.UUID4] = sqlmodel.Field(default=None, primary_key=True)  # type: ignore
+    id: Optional[pydantic.UUID1] = sqlmodel.Field(default=None, primary_key=True)  # type: ignore
     created_at: str = sqlmodel.Field(default_factory=datetime.utcnow, max_length=255)  # type: ignore
     message: str = sqlmodel.Field(max_length=255)  # type: ignore
     level: str = sqlmodel.Field(max_length=10)  # type: ignore
